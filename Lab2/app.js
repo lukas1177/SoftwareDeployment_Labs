ab06c3272e7e0809cbea58b36d211078534f4f5e
lukas1177 <117745@fhwn.ac.at>
2024-11-28 12:51:29 +0100

    created root

--- Lab2/app.js
+++ Lab2/app.js
@@ -7,6 +7,10 @@ const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+app.get('/', (req, res) => {
+  res.send('Root');
+});
+
 app.get('/api/hello', (req, res) => {
     res.send('Hello World');
   });
