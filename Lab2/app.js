3248464fa247634b827559141ad53bbf15057d0e
lukas1177 <117745@fhwn.ac.at>
2024-11-28 09:16:30 +0100

    updated app js

--- Lab2/app.js
+++ Lab2/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
 const app = express();
+const port = process.env.port || 3000;
 
-const server = app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 app.get('/hello', (req, res) => {
